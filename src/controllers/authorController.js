21b9375c783adc194b2f30da5969247cd446cc4f
AMAN8871266073 <atandon495@gmail.com>
2021-11-18 08:50:12 +0530

    assignment/schema3

--- src/controllers/authorController.js
+++ src/controllers/authorController.js
@@ -6,10 +6,13 @@ const createAuthor= async function (req, res) {
     res.send({msg: savedData})    
 }
 const bookByAuthor= async function (req, res) {
-    var data= await authorModel.find({"authorName":"Chetan Bhagat"}).select({"author_id":1})
-    let savedData= await bookModelTwo.find(data)
-    res.send({msg: savedData})    
+    var data= await authorModel.findOne({authorName:"Chetan Bhagat"}).select({author_id:1,_id:0})         //{authorName:"Chetan Bhagat"}).select({author_id:1,_id:0})
+   let savedData= await bookModelTwo.find(data)
+    res.send({msg:savedData})    
 }
 
+
 module.exports.createAuthor= createAuthor
-module.exports.bookByAuthor= bookByAuthor
+module.exports.bookByAuthor= bookByAuthor   
+
+
