479a7950b963af892ba592a0688d4fb4d0e386d8
AMAN8871266073 <atandon495@gmail.com>
2021-11-24 16:19:35 +0530

    p

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,9 +1,18 @@
 const headerValidation = function (req, res, next) {
     let acceptHeaderValue = req.headers.isfreeapp;
     console.log(acceptHeaderValue)
+    let isFreeApp
     if(acceptHeaderValue)
      {
     console.log('validation done succesfully')
+    if(acceptHeaderValue===false){
+        isFreeApp=false
+    }
+    else{
+        isFreeApp=true
+    }
+    req.isFreeAppUser=isFreeApp
+    console.log(isFreeAppUser)
      next()
      }
      else
